f3a19c362a9212ed6bf1e2557cdcbd54c40e921d
Rashed Arman <rashedarman21@gmail.com>
2023-05-20 23:47:15 +0600

    chore: fix linter errors

--- main.rb
+++ main.rb
@@ -1,20 +1,20 @@
 require_relative 'app'
 
 OPTIONS = {
-  "Please choose an option by entering a number:" => "",
-  "List all books" => :list_books,
-  "List all people" => :list_people,
-  "Create a person" => :create_person,
-  "Create a book" => :create_book,
-  "Create a rental" => :create_rental,
-  "List all rentals for a given person id" => :list_rentals,
-  "Exit" => :exit_app
+  'Please choose an option by entering a number:' => '',
+  'List all books' => :list_books,
+  'List all people' => :list_people,
+  'Create a person' => :create_person,
+  'Create a book' => :create_book,
+  'Create a rental' => :create_rental,
+  'List all rentals for a given person id' => :list_rentals,
+  'Exit' => :exit_app
 }.freeze
 
 def main
   app = App.new
 
-  puts "Welcome to School Library App!"
+  puts 'Welcome to School Library App!'
   selected_option = -1
 
   until selected_option == OPTIONS.length - 1
